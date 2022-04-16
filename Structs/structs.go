8edef5688cd9e873b36696c198a7584130d7e6e9
fillol <daniel_fillol@hotmail.com>
2022-04-16 11:28:42 -0300

    Add key tags on structs

--- Structs/structs.go
+++ Structs/structs.go
@@ -1,28 +1,28 @@
 package Structs
 
 type DocumentCNJ struct {
-	IdItem      string
-	IdItemUpper string
-	Name        string
+	IdItem      string `json:"IdItem,omitempty"`
+	IdItemUpper string `json:"IdItemUpper,omitempty"`
+	Name        string `json:"Name,omitempty"`
 }
 
 type RawDocument struct {
-	IdItem string
-	Name   string
+	IdItem string `json:"IdItem,omitempty"`
+	Name   string `json:"Name,omitempty"`
 }
 
 type FinalData struct {
-	Id         string
-	SearchName string
-	CNJId      string
-	CNJIdUpper string
-	CNJName    string
-	CnjReturn  []CNJArray
+	Id         string     `json:"Id,omitempty"`
+	SearchName string     `json:"SearchName,omitempty"`
+	CNJId      string     `json:"CNJId,omitempty"`
+	CNJIdUpper string     `json:"CNJIdUpper,omitempty"`
+	CNJName    string     `json:"CNJName,omitempty"`
+	CnjReturn  []CNJArray `json:"CnjReturn,omitempty"`
 }
 
 type CNJArray struct {
-	SearchName  string
-	IdItem      string
-	IdItemUpper string
-	Name        string
+	SearchName  string `json:"SearchName,omitempty"`
+	IdItem      string `json:"IdItem,omitempty"`
+	IdItemUpper string `json:"IdItemUpper,omitempty"`
+	Name        string `json:"Name,omitempty"`
 }
